756aaec0d3e91e3f0bf1bd84be29a1a0a573233d
foqjsblue <86902456+foqjsblue@users.noreply.github.com>
2021-07-22 15:13:01 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 #include <stddef.h> // for size_t
 #include <stdio.h> // for printf
 #include <stdint.h> // for uint8_t
+#include <netinet/in.h>
 
 int main(int argc, char* argv[])
+//uint32_t ntohl(uint32_t netlong)
 {
 	uint32_t a;
 	uint32_t b;
 
-	FILE *fpa = fopen(argv[1], "r");
-	FILE *fpb = fopen(argv[1], "r");
+	FILE *fpa;
+	FILE *fpb;
+
+	fpa = fopen(argv[1], "r");
+	fpb = fopen(argv[1], "r");
 
 	fread(&a, sizeof(a), 1, fpa);
-	fread(&a, sizeof(b), 1, fpb);
+	fread(&b, sizeof(b), 1, fpb);
 
+	//ntohl() : IP주소를 Network byte → Host byte 순서로
 	a = ntohl(a);
 	b = ntohl(b);
 
-	printf("%d", a+b);
+	printf("0x%x", a+b);
 }
